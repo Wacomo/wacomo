2ef45da47b42e9a9a2488aaa986b41f8123c4728
Melody <melody.amaizu@fhnw.ch>
2023-12-11 15:01:16 +0100

    stroryless task 48

--- Thingy-Client/src/app/interceptors/backend.interceptor.ts
+++ Thingy-Client/src/app/interceptors/backend.interceptor.ts
@@ -1,19 +1,27 @@
-import { HttpInterceptorFn } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { HttpRequest, HttpResponse, HttpHandler, HttpEvent, HttpInterceptor, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { Observable, of, throwError } from 'rxjs';
-
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment.prod';
 
-
 @Injectable()
-export class BackendInterceptor implements HttpInterceptor  {
+export class BackendInterceptor implements HttpInterceptor {
+  constructor() { }
 
-    constructor() { }
-
-    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        const baseUrl = environment.backendbaseURL;
-        const apiReq = request.clone({ url: `${baseUrl}/${request.url}` });
-        return next.handle(apiReq);
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    // Check if the request URL is for an external API
+    if (!this.isExternalApi(request.url)) {
+      // Append the base URL for internal API requests
+      const baseUrl = environment.backendbaseURL;
+      const apiReq = request.clone({ url: `${baseUrl}/${request.url}` });
+      return next.handle(apiReq);
     }
+    // For external API requests, do not modify the URL
+    return next.handle(request);
+  }
+
+  private isExternalApi(url: string): boolean {
+    // Add other external API base URLs as needed
+    const externalApis = ['https://api.openai.com'];
+    return externalApis.some(apiUrl => url.startsWith(apiUrl));
+  }
 }
--- Thingy-Client/src/environments/environment.prod.ts
+++ Thingy-Client/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
     backendbaseURL:'http://localhost:3900/api/v1',
+    openaiApiKey: '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',
   };
